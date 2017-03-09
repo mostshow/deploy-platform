e5e945655d6ca9d6009f31b5b38c674ca0565e12
lyndon <mostshow@163.com>
2017-03-09 20:36:40 +0800

    test server

--- src/js/constants/ServerTypes.js
+++ src/js/constants/ServerTypes.js
@@ -1,4 +1,5 @@
 //api
-export const API_ROOT = 'http://localhost:3002'
+// export const API_ROOT = 'http://localhost:3002'
+export const API_ROOT = 'http://10.16.15.101:3002'
 export const UPLOAD_URL = 'http://localhost:3002/api/image/create'
 export const API_FILTER = ['']
