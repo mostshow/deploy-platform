d6dabdd09d1278071ddde261e4180d6c6960940c
lyndon <mostshow@163.com>
2017-03-17 10:42:13 +0800

    use hashHistory

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -4,6 +4,6 @@ import { browserHistory ,hashHistory} from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 export const api = _api
-export const history = browserHistory
+export const history = hashHistory
 //browserHistory;
-export const createHistory = (store)=>syncHistoryWithStore(browserHistory,store);
+export const createHistory = (store)=>syncHistoryWithStore(hashHistory,store);
